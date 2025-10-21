3c7cd1c10f48bf9e97b9007e36d60c95d08355a6
Emre Üçbudak <emreucbudak1905@gmail.com>
2025-10-22 01:28:43 +0300

    Proje dosyası ekle.

---
+++ RestoranManagementinFastApi/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RestoranManagementinFastApi")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "RestoranManagementinFastApi.Pages")]
---
+++ RestoranManagementinFastApi/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RestoranManagementinFastApi
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
